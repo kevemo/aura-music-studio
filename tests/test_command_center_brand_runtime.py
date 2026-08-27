1b60f5d6f303d77fcf8f2c15d30ca27ca4ebff4f
kevemo <43649141+kevemo@users.noreply.github.com>
2026-08-27 14:01:32 +0100

    Align brand runtime regression with current logo compatibility

--- tests/test_command_center_brand_runtime.py
+++ tests/test_command_center_brand_runtime.py
@@ -3,7 +3,7 @@ from fastapi.testclient import TestClient
 
 from aura_music_studio.aura_context_extensions import _inject_messages
 from aura_music_studio.brand_migration import BrandMigrationMiddleware, rebrand_text
-from aura_music_studio.branding import BRAND_ART_ROUTE, ENDORSEMENT, PRODUCT_FULL_NAME
+from aura_music_studio.branding import BRAND_ART_ROUTE, BRAND_LOGO_PATH, ENDORSEMENT, PRODUCT_FULL_NAME
 
 
 def test_aura_receives_current_identity_before_inference_without_profile_extensions():
@@ -33,12 +33,12 @@ def test_private_tool_router_prompt_is_not_rewritten():
     assert _inject_messages(messages, "member-1", "thread-1") is messages
 
 
-def test_retired_logo_urls_rewrite_to_current_master_art_route():
+def test_retired_logo_urls_rewrite_to_current_mark_route():
     branded = rebrand_text(
         "<img src='/brand/pulsar-frequency-house-logo.svg'>"
         "<img src='/static/pulsar-frequency-house-logo.svg'>"
     )
-    assert branded.count(BRAND_ART_ROUTE) == 2
+    assert branded.count(BRAND_LOGO_PATH) == 2
     assert "pulsar-frequency-house-logo.svg" not in branded
 
 
