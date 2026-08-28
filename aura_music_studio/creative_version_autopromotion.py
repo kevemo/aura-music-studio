d02d8dc6b0185562d065f5a44e085c8cd69878b7
kevemo <43649141+kevemo@users.noreply.github.com>
2026-08-28 16:40:04 +0100

    Video: mount project music synchronization routes

--- aura_music_studio/creative_version_autopromotion.py
+++ aura_music_studio/creative_version_autopromotion.py
@@ -33,6 +33,7 @@ from .stripe_billing import router as stripe_billing_router
 from .stripe_billing_hardening import router as stripe_billing_hardening_router
 from .stripe_commerce_receipts import router as stripe_commerce_receipts_router
 from .tenant_storage import project_path
+from .video_music_sync import router as video_music_sync_router
 from .video_scene_local_revision import router as video_scene_local_revision_router
 from .video_scene_render import router as video_scene_render_router
 from .video_scene_timeline import router as video_scene_timeline_router
@@ -64,6 +65,7 @@ router.include_router(video_scene_timeline_router)
 router.include_router(video_visual_continuity_router)
 router.include_router(video_scene_render_router)
 router.include_router(video_scene_local_revision_router)
+router.include_router(video_music_sync_router)
 router.include_router(professional_editor_router)
 router.include_router(professional_editor_lifecycle_router)
 router.include_router(professional_editor_render_router)
